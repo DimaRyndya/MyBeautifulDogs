4ecd301fbbfe97ffcc3b63fb1fc8aa33766dd9d2
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-09-15 22:11:13 +0300

    Initial Commit

---
+++ MyBeautifulDogs/MyBeautifulDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBeautifulDogsApp.swift
+//  MyBeautifulDogs
+//
+//  Created by Dmitriy Ryndya on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyBeautifulDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
