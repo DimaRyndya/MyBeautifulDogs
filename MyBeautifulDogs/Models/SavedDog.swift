2317c9b461da6001d539e5bfb7d62ec4b00ad4c3
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-09-24 15:30:57 +0300

    l

--- MyBeautifulDogs/Models/SavedDog.swift
+++ MyBeautifulDogs/Models/SavedDog.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct SavedDog {
-//    var id = UUID()
     var dogName = ""
     var breedName: BreedName?
 
@@ -17,14 +16,11 @@ extension SavedDog: Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         dogName = try container.decode(String.self, forKey: .dogName)
         breedName = try container.decode(BreedName.self, forKey: .breedName)
-//        let id = try container.decode(String.self, forKey: .id)
-//        self.id = UUID(uuidString: id) ?? UUID()
     }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(dogName, forKey: .dogName)
         try container.encode(breedName, forKey: .breedName)
-//        try container.encode(id, forKey: .id)
     }
 }
