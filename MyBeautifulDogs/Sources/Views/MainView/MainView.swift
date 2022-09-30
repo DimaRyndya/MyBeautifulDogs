46751646a040d9f932308b619801001663fdabb9
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-09-30 20:52:10 +0300

    add enum Configurations

--- MyBeautifulDogs/Sources/Views/MainView/MainView.swift
+++ MyBeautifulDogs/Sources/Views/MainView/MainView.swift
@@ -1,5 +1,11 @@
 import SwiftUI
 
+enum Configuraions {
+    static let dogsCards = "Dogs Cards"
+    static let dogsList = "Dogs List"
+    static let myDogs = "My Dogs"
+}
+
 struct MainView: View {
     @EnvironmentObject var viewState: ViewState
     
@@ -10,19 +16,19 @@ struct MainView: View {
                     DogsCardsView()
                         .tabItem {
                             Image(systemName: "house")
-                            Text("Dogs Cards")
+                            Text(Configuraions.dogsCards)
                         }
                         .tag(1)
                     CardsListView()
                         .tabItem {
                             Image(systemName: "list.bullet")
-                            Text("Dogs List")
+                            Text(Configuraions.dogsList)
                         }
                         .tag(2)
                     MyDogsView()
                         .tabItem {
                             Image(systemName: "suit.heart")
-                            Text("My Dogs")
+                            Text(Configuraions.myDogs)
                         }
                         .tag(3)
                 }
